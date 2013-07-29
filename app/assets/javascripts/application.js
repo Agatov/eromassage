ee9d149f9c90120c0db8a755aada91b8414645a6
Boris Kuznetsov <achempion@gmail.com>
2013-07-29 16:48:32 +0400

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,19 +1,6 @@
 //= require lib/jquery-1.9.1
 //= require jquery-ui
-
 //= require application/girl_profile
 //= require application/initializer
 //= require application/order
 //= require application/guestbook
-
-
-// require lib/underscore
-// require lib/backbone
-
-// require eromassage
-
-// require_tree ../templates
-// require_tree ./models
-// require_tree ./collections
-// require_tree ./views
-// require_tree ./routers
